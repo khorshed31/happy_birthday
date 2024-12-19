721a3ac8f0e45a85b2b9d1f3022b0a2143306780
khorshed31 <khorshedalom3517@gmail.com>
2024-12-19 22:36:59 +0600

    upload

---
+++ config.js
@@ -0,0 +1,26 @@
+var config = {
+    texts: [
+        "Send from Khorshed Alom",      
+        "Count your life by smiles, not tears",      
+        "Count your age by friends, not years",      
+        "Beloved Respected Boro APU", 
+        "Today is your birthday",
+        "Wish you happy returns of the day",
+        "Alles Gute zum Geburtstag 🇩🇪",
+        "শুভ পয়দা দিবস",
+        "Happy Downloading... Day",
+        "Best German Language Teacher",
+        "Lots Of love",
+    ],
+    
+    desc: {
+        turn_on: "Turn On Lights",
+        play: "Play Music",
+        bannar_coming: "Let's Decorate",
+        balloons_flying: "Fly With Balloons",
+        cake_fadein: "Most Delicious Cake Ever",
+        light_candle: "Light Candle",
+        wish_message: "Happy Birthday",
+        story: "MESSAGES FOR YOU",
+    }
+};
